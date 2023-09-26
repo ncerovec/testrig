f14dfe07f025e0025c477fa0aa1b3c653cbd77be
nino888 <nino.cerovec@hotmail.com>
2023-09-26 20:07:49 +0200

    testrig - snapshot version 1.0.0 (Java 17)

---
+++ src/main/java/io/github/ncerovec/testrig/HelloWorld.java
@@ -0,0 +1,15 @@
+package io.github.ncerovec.testrig;
+
+/**
+ * Hello World example class
+ */
+public class HelloWorld
+{
+    /**
+     * Method prints "Hello World" to standard output
+     */
+    public static void helloWorld()
+    {
+        System.out.println("Hello World");
+    }
+}
